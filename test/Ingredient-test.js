d89413959927af5f09c4779d692d25055d4f8e71
Daniel Neer <daniel.r.neer@gmail.com>
2022-03-29 14:41:22 -0700

    Add ingredient test suite

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -14,7 +14,68 @@ const recipes = require('../src/data/recipes.js');
 const users = require('../src/data/users.js');
 
 describe('Ingredient', () => {
+    let ingredient
+    beforeEach(() => {
+        ingredient = new Ingredient(1, "Potatoe", 99)
+    })
+
     it('Should be a function', () => {
         expect(Ingredient).to.be.a('function');
     });
-})
+
+    it('Should be an instance of ingedient', () => {
+        expect(ingredient).to.be.instanceOf(Ingredient)
+    });
+
+    it('Should store a id number', () => {
+        expect(ingredient.id).to.equal(1)
+    });
+        
+    it('Should store a name string', () => {
+        expect(ingredient.name).to.equal("Potatoe")
+    })
+
+    it('Should store a estimatedCostInCents number', () => {
+        expect(ingredient.estimatedCostInCents).to.equal(99)
+    });
+
+    it('Should store a igredient unit string', () => {
+        expect(ingredient.unit).to.equal("servings")
+    });
+
+    it('Should store a ingredient amount number', () => {
+        expect(ingredient.amount).to.equal(1)
+    });
+
+    it('Should have a method to return id', () => {
+        expect(ingredient.getId()).to.equal(1)
+    })
+
+    it('Should have a method to return name', () => {
+        expect(ingredient.getName).to.equal('Potatoe') 
+    })
+
+    it('Should have a method to return estimatedCostInCents', () => {
+        expect(ingredient.getCost).to.equal(99)
+    })
+
+    it('Should have a method to return ingredient unit', () => {
+        expect(ingredient.getUnit).to.equal("servings")
+    })
+
+    it('Should have a method to return ingredient amount', () => {
+        expect(ingredient.getAmount).to.equal(1)
+    })
+
+    it.skip('Should have a method to update amount', () => {
+        expect(ingredient.updateAmount).to.equal()
+    })
+
+    it.skip('Should be able to update the name', () => {
+        expect(ingredient.estimatedCostInCents).to.equal(99)
+    })
+
+    it.skip('Should be able to update the estimatedCostInCents', () => {
+        expect(ingredient.estimatedCostInCents).to.equal(99)
+    })
+});
