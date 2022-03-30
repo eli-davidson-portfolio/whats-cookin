5a970e65e60d5b15167cd7a0d7bc5e708ac02e05
Daniel Neer <daniel.r.neer@gmail.com>
2022-03-30 14:49:19 -0700

    close #33

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -14,7 +14,41 @@ const recipes = require('../src/data/recipes.js');
 const users = require('../src/data/users.js');
 
 describe('RecipeRepository', () => {
+  let rr
+
+  beforeEach(() => {
+    rr = new RecipeRepository()
+  })
+
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
+  });
+
+  it('Should be an instance of RecipeRepository', () => {
+    expect(rr).to.be.an.instanceOf(RecipeRepository);
+  });
+
+  it('Should be able to store a data array', () => {
+    expect(rr.data).to.be.an("array");
+  });
+
+  it('Should be a method to instantiate recipe objects', () => {
+    expect(rr.createRecipe).to.be.an("function");
+  });
+
+  it('Should store an array of recipe objects', () => {
+    expect(rr.recipes).to.be.an("array");
+  });
+
+  it('Should have a method to return recipes', () => {
+    expect(rr.getRecipes).to.be.a("function");
+  });
+
+  it('Should be able to filter recipes by tag', () => {
+    expect(rr.filterTag).to.be.a("function");
+  });
+
+  it('Should be able to filter recipes by name', () => {
+    expect(rr.filterName).to.be.a("function");
   });
 })
