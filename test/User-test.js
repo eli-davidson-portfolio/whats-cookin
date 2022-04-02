b8c38aabf6d132da4dbc67cbe8c8703a29b2204a
Daniel Neer <daniel.r.neer@gmail.com>
2022-04-02 14:10:59 -0700

    close #37

--- test/User-test.js
+++ test/User-test.js
@@ -1,20 +1,83 @@
-// import { expect } from 'chai';
+import { expect } from 'chai';
 // //Classes
-// const Ingredient = require('../src/classes/Ingredient.js');
-// const Recipe = require('../src/classes/Recipe.js');
-// const RecipeRepository = require('../src/classes/RecipeRepository.js');
-// const User = require('../src/classes/User.js');
-// const Pantry = require('../src/classes/Pantry.js');
-// const GroceryList = require('../src/classes/GroceryList.js');
-// const FavoriteList = require('../src/classes/FavoriteList.js');
-// const ToCookList = require('../src/classes/ToCookList.js');
-// //Data
-// const ingredients = require('../src/data/ingredients.js');
-// const recipes = require('../src/data/recipes.js');
-// const users = require('../src/data/users.js');
-//
-// describe('User', () => {
-//     it('Should be a function', () => {
-//         expect(User).to.be.a('function');
-//     });
-// })
+import User from '../src/classes/User.js'
+
+describe('Users', () => {
+
+    let user = new User()
+    
+    beforeEach(() => {
+        user = new User()
+    })
+  
+      it('Should be a function', () => {
+          expect(User).to.be.a('function');
+      });
+  
+      it('Should be an instance of recipe', () => {
+          expect(user).to.be.instanceOf(User)
+      });
+
+      it('Should have a method to return id', () => {
+        expect(user.getId()).to.equal(1)
+    });
+
+    it('Should have a method to return name', () => {
+        expect(user.getName()).to.be.equal("Saige O'Kon")
+    });
+
+    it('Should have a method to return pantry items', () => {
+        expect(user.getPantryItems()).to.be.an('array')
+        expect(user.getPantryItems()[0].ingredient).to.equal(11297)
+    });
+
+    it('Should have a method to return favorties', () => {
+        user.addFavorite(12345)
+        expect(user.getFavorites()[0]).to.equal(12345)
+    });
+
+    it('Should have a method to return recipes to cook', () => {
+        user.addToCook(54321)
+        expect(user.getRecipesToCook()[0]).to.equal(54321)
+    });
+
+      it('Should store an id number', () => {
+        expect(user.id).to.be.a("number")
+    });
+    it('Should store a name string', () => {
+        expect(user.name).to.be.a("string")
+    });
+
+    it('Should store an array of pantry items', () => {
+        expect(user.pantryItems).to.be.a("array")
+    });
+
+    it('Should store and array of favorite recipes', () => {
+        expect(user.favorites).to.be.an("array")
+    });
+
+    it('Should store and array of recipes to cook', () => {
+        expect(user.recipesToCook).to.be.an("array")
+    });
+
+    it('Should have a method to get the pantry item amount', () => {
+        let result = user.getPantryItemAmount(1082047)
+        expect(result).to.equal(10)
+    });
+
+    it('Should have a method to add and remove id from favorites array', () => {
+        user.addFavorite(12345)
+        expect(user.favorites.length).to.equal(1)
+        expect(user.favorites[0]).to.equal(12345)
+        user.removeFavorite(12345)
+        expect(user.favorites.length).to.equal(0)
+    });
+
+    it('Should have a method to add and remove id from recipesToCook array', () => {
+        user.addToCook(12345)
+        expect(user.recipesToCook.length).to.equal(1)
+        expect(user.recipesToCook[0]).to.equal(12345)
+        user.removeToCook(12345)
+        expect(user.recipesToCook.length).to.equal(0)
+    });
+});
