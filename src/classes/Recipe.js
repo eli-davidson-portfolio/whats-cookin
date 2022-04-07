6fbf12e9a1df7662bec27d4071cf41fa38f3cadd
Eli Davidson <eli@shadowlab.dev>
2022-04-07 17:28:31 -0600

    Refactor update cost method adding guard clause

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -27,48 +27,36 @@ class Recipe {
     }
 
     updateCost() {
-        this.totalCost = this.ingredients.reduce((sum, ingredient) => {
-            sum += ingredient.estimatedCostInCents
-            return sum
-        }, 0)
-        this.totalCost = this.totalCost / 100
-        this.UpdateIngredientWords()
+      if (!this.ingredients.length) return;
+
+      this.totalCost = this.ingredients.reduce((sum, ingredient) => {
+          sum += ingredient.estimatedCostInCents;
+          return sum;
+      }, 0);
+
+      this.totalCost = this.totalCost / 100;
+      this.UpdateIngredientWords();
     }
 
-    // favorite() {
-    //    if (!this.tags.includes('favorite')) this.tags.push('favorite')
-    // }
-
-    // unfavorite() {
-    //     if (this.tags.includes('favorite')) this.tags.pop()
-    // }
-
-    // toCook() {
-    //     if (!this.tags.includes('toCook')) this.tags.unshift('toCook')
-    // }
-
-    //  notToCook() {
-    //      if (this.tags.includes('toCook')) this.tags.shift()
-    //  }
       getIngredientwords() {
         return this.ingredientWords
       }
-
 
      getIngredientNames() {
          return this.ingredients.map((ingredient) => {
              return ingredient.name
          })
      }
-    UpdateIngredientWords() {
-     this.ingredients.forEach((ingredient) => {
-        ingredient.name.split(' ').forEach(word => {
-          if (!this.ingredientWords.includes(word)) {
-            this.ingredientWords.push(word)
-          }
+
+      UpdateIngredientWords() {
+      this.ingredients.forEach((ingredient) => {
+          ingredient.name.split(' ').forEach(word => {
+            if (!this.ingredientWords.includes(word)) {
+              this.ingredientWords.push(word)
+            }
+          })
         })
-      })
-      this.ingredientWords.sort();
-    }
+        this.ingredientWords.sort();
+      }
 }
 export default Recipe;
